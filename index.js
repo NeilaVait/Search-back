e9ec39ef81051c69c6e9c1ba1c0060440c8e90dd
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-11-25 15:22:11 +0200

    connection to mongo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_CONN_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Conneced to mongoose');
+  })
+  .catch((err) => console.error(err.message));
 
 app.listen(PORT, console.log(`back end online on port ${PORT}`));
